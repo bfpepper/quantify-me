c2debab6021ec58d37e929c39544741f491316ec
Ben Pepper <bfpepper87@gmail.com>
2017-01-26 16:24:25 -0700

    food and exercises presist to diary page.

--- test/user-creates-an-exercise-entry-test.js
+++ test/user-creates-an-exercise-entry-test.js
@@ -13,13 +13,13 @@ test.describe("user creates an exercise entry from exercises.html", function(){
   })
 
   test.afterEach(function(){
-    driver.get('http://localhost:8080/webpack-dev-server/exercises.html')
+    driver.get('http://localhost:8080/exercises.html')
     driver.executeScript("window.localStorage.clear()");
     driver.quit();
   })
 
   test.it("prints updated information to page", function(){
-    driver.get("http://localhost:8080/webpack-dev-server/exercises.html");
+    driver.get("http://localhost:8080/exercises.html");
     driver.sleep(1000)
     var name = driver.findElement({id: 'create-exercise-name'});
     var calories = driver.findElement({id: 'create-exercise-calorie-count'});
