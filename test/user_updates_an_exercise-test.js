d5460ac9f2d49f227197ca684b7746d05ec0d052
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-26 16:23:49 -0700

    User updates exercise test passing

--- test/user_updates_an_exercise-test.js
+++ test/user_updates_an_exercise-test.js
@@ -2,39 +2,39 @@ var assert    = require('chai').assert;
 var webdriver = require('selenium-webdriver');
 var test      = require('selenium-webdriver/testing');
 
-test.describe("user sees their exercises on exercises.html", function(){
+test.describe("user updates an exercise on exercise.html", function(){
   var driver;
-  this.timeout(1000);
+  this.timeout(100000);
 
   test.beforeEach(function(){
     driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
   });
+
   test.afterEach(function(){
     driver.get('http://localhost:8080/exercises.html');
     driver.executeScript('window.localStorage.clear');
     driver.quit();
   });
 
-  test.it('add exercise into localStorage', function(){
+  test.it('updates an exercise inline', function(){
     driver.get('http://localhost:8080/exercises.html');
     var exercisesJSON = JSON.stringify([{name:'hop', calories:'20'}])
     driver.executeScript("window.localStorage.setItem('exercises', '" + exercisesJSON + "')");
 
-    driver.get("http://localhost:8080/exercises.html");
-    driver.executeScript("return window.localStorage.getItem('exercises')")
-    .then(function(exercise){
-      assert.equal(exercise, '[{"name":"hop","calories":"20"}]');
-    });
+    driver.get('http://localhost:8080/exercises.html');
+    var exerciseName = driver.findElement({css: 'table td'});
+    var exerciseCal = driver.findElement({css: 'table td:nth-child(2)'});
+    var elsewhere = driver.findElement({id: 'create-exercise-name'})
 
-    driver.get("http://localhost:8080/exercises.html");
+    exerciseName.click();
+    var updateName = driver.findElement({css: 'table td input'});
+    updateName.sendKeys('s');
+    elsewhere.click();
+    
     driver.findElement({css: 'table td'}).getText().then(function(textValue){
-      assert.include(textValue, 'hop')
+      assert.equal(textValue,'hops')
     });
-    driver.findElement({css: 'table td:nth-child(2)'}).getText().then(function(textValue){
-      assert.include(textValue, '20')
-    });
-
   });
 });
