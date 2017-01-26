8e256653291d0af810afa9f7151835b60ebd094c
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-25 18:32:28 -0700

    Add create exercise feature test, not passing

---
+++ test/exercise-test.js
@@ -0,0 +1,12 @@
+// var assert = require('chai').assert;
+// var exercise = require('../lib/exercises')
+//
+// describe("exercises", function(){
+//   context("saveExercise", function(){
+//     it("updates localStorage", function(){
+//       var newExercise = new exercise;
+//       newExercise.saveExercise('hop', '20')
+//       assert.equal(localStorage, {name: 'name', calories: '20'})
+//     })
+//   })
+// })
---
+++ test/user-creates-an-exercise-entry-test.js
@@ -0,0 +1,36 @@
+var assert = require('chai').assert;
+var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+
+test.describe("user creates an exercise entry from exercises.html", function(){
+  var driver;
+  this.timeout(10000);
+
+  test.beforeEach(function(){
+    driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+  })
+
+  test.afterEach(function(){
+    driver.get('http://localhost:8080/webpack-dev-server/exercises.html')
+    driver.executeScript("window.localStorage.clear()");
+    driver.quit
+  })
+
+  test.it("prints updated information to page", function(){
+    driver.get("http://localhost:8080/webpack-dev-server/exercises.html");
+    driver.sleep(1000)
+    var name = driver.findElement({id: 'create-exercise-name'});
+    var calories = driver.findElement({id: 'create-exercise-calorie-count'});
+
+    name.sendKeys('hop');
+    name.getAttribute('value').then(function(value){
+      assert.equal(value, 'hop');
+    });
+    calories.sendKeys('20');
+    calories.getAttribute('value').then(function(value){
+      assert.equal(value, '20');
+    });
+  });
+});
